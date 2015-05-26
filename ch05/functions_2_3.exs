a66cb781931b45f0976cc4531a0d146541f63d75
Daniel Garcia <pel_daniel@hotmail.com>
2015-05-26 06:19:35 -0500

    Functions 2 & 3

---
+++ ch05/functions_2_3.exs
@@ -0,0 +1,15 @@
+f2 = fn
+  (0, 0, _) -> "FizzBuzz"
+  (0, _, _) -> "Fizz"
+  (_, 0, _) -> "Buzz"
+  (_, _, n) -> n
+end
+
+IO.inspect f2.(0, 0, 0)
+IO.inspect f2.(0, 1, 2)
+IO.inspect f2.(1, 0, 3)
+IO.inspect f2.(1, 2, 3)
+
+fizzbuzz = fn n -> f2.(rem(n, 3), rem(n, 5), n) end
+
+Enum.each 10..16, fn(x) -> IO.inspect fizzbuzz.(x) end
