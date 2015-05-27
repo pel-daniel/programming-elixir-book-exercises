7e547c2418914834fbb47ea5f292a419630d572e
Daniel Garcia <pel_daniel@hotmail.com>
2015-05-27 07:27:19 -0500

    MyList.caesar/2

--- ch07/my_list.exs
+++ ch07/my_list.exs
@@ -12,4 +12,6 @@ defmodule MyList do
   defp _max([], acc), do: acc
   defp _max([head | tail], acc) when head > acc, do: _max(tail, head)
   defp _max([head | tail], acc) when head <= acc, do: _max(tail, acc)
+
+  def caesar(list, n), do: map(list, &(rem(&1 + n - 97, 26) + 97))
 end
