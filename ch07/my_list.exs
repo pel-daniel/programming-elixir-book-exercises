a0e331893480266ac5806eea53ed0a3bf2b24cd1
Daniel Garcia <pel_daniel@hotmail.com>
2015-05-27 06:57:57 -0500

    MyList.mapsum/2

--- ch07/my_list.exs
+++ ch07/my_list.exs
@@ -1,4 +1,9 @@
 defmodule MyList do
   def sum([]), do: 0
   def sum([head|tail]), do: head + sum(tail)
+
+  def map([], _func), do: []
+  def map([head|tail], func), do: [func.(head) | map(tail, func)]
+
+  def mapsum(list, func), do: sum(map(list, func))
 end
