bcfe7d699801997c7b50b14cdb22a832cd3b9253
Daniel Garcia <pel_daniel@hotmail.com>
2015-06-05 05:22:53 -0500

    ch10 MyEnum.all?/2, MyEnum.each/2, MyEnum.filter/2, MyEnum.split/2 & MyEnum.take/2

---
+++ ch10/my_enum.exs
@@ -0,0 +1,31 @@
+defmodule MyEnum do
+  def all?([], _func), do: true
+  def all?([head | tail], func), do: func.(head) and all?(tail, func)
+  
+  def each([], _func), do: :ok
+  def each([head | tail], func) do
+    func.(head)
+    each(tail, func)
+  end
+
+  def filter([], _func), do: []
+  def filter([head | tail], func) do
+    if func.(head) do
+      [head | filter(tail, func)]
+    else
+      filter(tail, func)
+    end
+  end
+
+  def split(ls, n), do: _split(ls, n, [])
+
+  def _split([], _n, acc), do: { Enum.reverse(acc), [] }
+  def _split(ls, 0, acc),   do: { Enum.reverse(acc), ls }
+  def _split([head | tail], n, acc), do: _split(tail, n-1, [head | acc])
+
+  def take([], _n), do: []
+  def take(_, 0),  do: []
+  def take([head | tail], n) do
+    [head | take(tail, n - 1)]
+  end
+end
