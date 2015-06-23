79237b00e32d62acedcb9c2c32433cd2e1614f03
Daniel Garcia <pel_daniel@hotmail.com>
2015-06-23 03:56:02 -0500

    ch12 FizzBuzz.upto/1 and FizzBuzz.fizzbuzz/1

---
+++ ch12/fizz_buzz.exs
@@ -0,0 +1,12 @@
+defmodule FizzBuzz do
+  def upto(n), do: 1..n |> Enum.map(&fizzbuzz/1)
+
+  def fizzbuzz(n) do
+    case {rem(n, 3), rem(n, 5)} do
+      {0, 0} -> "FizzBuzz"
+      {0, _} -> "Fizz"
+      {_, 0} -> "Buzz"
+      _         -> n
+    end
+  end
+end
